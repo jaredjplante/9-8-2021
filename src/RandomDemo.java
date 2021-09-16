d3d82ff81abee5680f9418e9f1124cfd469f3a8e
jared <j2plante@student.bridgew.edu>
2021-09-16 00:39:56 -0400

    demo random number final

--- src/RandomDemo.java
+++ src/RandomDemo.java
@@ -8,11 +8,22 @@ public class RandomDemo {
         var inputReader = new Scanner(System.in);
         System.out.print("pick a random number from 1 to 10:");
         var usersNumber = inputReader.nextInt();
+        while(true){
+            //while the if statements are true
         if(randomNumber == usersNumber){
             System.out.println("Your were right the number was "+randomNumber);
+            break;
         }
-        else{
-            System.out.println("Your number was incorrect");
+            if(randomNumber > usersNumber){
+                System.out.println("Your number was too low, try again");
+                System.out.print("pick a random number from 1 to 10:");
+                usersNumber = inputReader.nextInt();
+        }
+            if(randomNumber < usersNumber){
+                System.out.println("Your number was too high, try again");
+                System.out.print("pick a random number from 1 to 10:");
+                usersNumber = inputReader.nextInt();
+            }
         }
     }
 }
